85566678ecabb1b92fc42665555e73ad4df732b1
Ryan Nowak <nowakra@gmail.com>
2018-01-22 22:21:55 -0800

    Add DNS stuff

---
+++ Dns/Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddHttpClient("dns");
+
+            var factory = services.BuildServiceProvider().GetRequiredService<IHttpClientFactory>();
+
+            var client = factory.CreateClient("dns");
+
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (sender, e) => cts.Cancel();
+
+            while (!cts.IsCancellationRequested)
+            {
+                var content = await client.GetStringAsync("http://192.168.1.104:5000/");
+                Console.WriteLine(content);
+
+                await Task.Delay(300);
+            }
+        }
+    }
+}
---
+++ Dns/Server/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult<object> Get()
+        {
+            return new { message = $"Hello from, {Environment.MachineName}", };
+        }
+    }
+}
